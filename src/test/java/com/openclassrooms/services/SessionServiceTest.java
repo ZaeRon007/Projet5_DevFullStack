59d6c5d8bc3c9504d6fe75bad95a2fbaccd57bc4
ZaeRon007 <julien.pelissier8@orange.fr>
2024-09-21 12:19:03 +0200

    Work In Progress : sessionServiceTest

---
+++ src/test/java/com/openclassrooms/services/SessionServiceTest.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.services;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SessionServiceTest {
+    
+    @Test
+    public void a(){
+    }
+}

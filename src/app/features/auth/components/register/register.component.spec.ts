03132bc9ff66aa6cdc1a70d7f726ff7d04cbe3cd
ZaeRon007 <julien.pelissier8@orange.fr>
2024-10-06 20:37:50 +0200

    register.component test working

--- src/app/features/auth/components/register/register.component.spec.ts
+++ src/app/features/auth/components/register/register.component.spec.ts
@@ -1,31 +1,32 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { expect } from '@jest/globals';
-
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 import { RegisterComponent } from './register.component';
+import { of } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let mockAuthService: any;
+  let mockRouter: any;
 
   beforeEach(async () => {
+    mockAuthService = {
+      register: jest.fn()
+    };
+
+    mockRouter = {
+      navigate: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
-      imports: [
-        BrowserAnimationsModule,
-        HttpClientModule,
-        ReactiveFormsModule,  
-        MatCardModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule
-      ]
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: Router, useValue: mockRouter }],
+      imports: [ReactiveFormsModule]
     })
       .compileComponents();
 
@@ -36,5 +37,42 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form', () => {
+    fixture.detectChanges();
+
+    const form = component.form;
+    expect(form.contains('email')).toBe(true);
+    expect(form.contains('firstName')).toBe(true);
+    expect(form.contains('lastName')).toBe(true);
+    expect(form.contains('password')).toBe(true);
+
+  });
+
+  it('should register and navigate to /login', () => {
+    const mockResponse = {
+      email: 'john@gmail.com',
+      firstName: "john",
+      lastName: "doe", 
+      password: 'password'
+    };
+
+    mockAuthService.register.mockReturnValue((of(mockResponse)));
+
+    component.form.setValue({email: 'john@gmail.com',
+                            firstName: "john",
+                            lastName: "doe", 
+                            password: 'password'});
+
+    component.submit();
+
+    expect(mockAuthService.register).toHaveBeenCalledWith({email: 'john@gmail.com',
+                                                          firstName: "john",
+                                                          lastName: "doe", 
+                                                          password: 'password'});
+
+
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/login']);
   });
 });

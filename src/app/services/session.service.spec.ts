8fc8a0b848a6cbc9ee4fb81f04ece85440dccf82
ZaeRon007 <julien.pelissier8@orange.fr>
2024-10-06 16:50:25 +0200

    sessionService test working

--- src/app/services/session.service.spec.ts
+++ src/app/services/session.service.spec.ts
@@ -2,25 +2,61 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 import {HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
-  let service: SessionService;
-  // let httpMock: HttpTestingController;
+  let sessionService: SessionService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [SessionService]
     });
 
-    service = TestBed.inject(SessionService);
+    sessionService = TestBed.inject(SessionService);
+    httpMock = TestBed.inject(HttpTestingController);
+
   });
 
-  // afterEach(() => {
-  //   httpMock.verify();
-  // })
+  afterEach(() => {
+    httpMock.verify();
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(sessionService).toBeTruthy();
   });
+
+  it('should log in user', () => {
+    let expectedSession: SessionInformation = {
+      token: "ad52gf456j6k98h2g1de489y32csw45xw5c6nh9ik9i",
+      type: "type",
+      id: 1,
+      username: "pedro@gmail.com",
+      firstName: "john",
+      lastName: "doe",
+      admin: false}
+
+    sessionService.$isLogged().subscribe((response) => {
+      expect(response).toBe(true);
+    })  
+
+    sessionService.logIn(expectedSession);
+
+    expect(sessionService.sessionInformation).toEqual(expectedSession);
+    expect(sessionService.isLogged).toBe(true);
+
+  })
+
+  it('should log out user', () => {
+    sessionService.$isLogged().subscribe((response) => {
+      expect(response).toBe(false);
+    })  
+
+    sessionService.logOut();
+
+    expect(sessionService.sessionInformation).toBe(undefined);
+    expect(sessionService.isLogged).toBe(false);    
+  })
 
 });
